66d59233bb475bef072452494fcd16bdfafd2b69
adasilvamart <adasilva.clase@gmail.com>
2024-03-08 13:58:35 +0100

    test(test): :test_tube: Create Tarifa.getCosteTotalEsperado test

---
+++ app/src/test/java/edu/badpals/cotxox/tarifa/TarifaTest.java
@@ -0,0 +1,39 @@
+package edu.badpals.cotxox.tarifa;
+import org.junit.Test;
+
+import edu.badpals.cotxox.carrera.Carrera;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;    
+    
+public class TarifaTest {
+    private Carrera carrera;
+    
+    @Before
+    public void setup(){
+        /* 
+		 * Configuracion del usuario
+		 * y de la Carrera. 
+		 */
+
+		String tarjetaCredito = "4916119711304546";
+		String origen = "Aeroport Son Sant Joan";
+		String destino = "Magaluf";
+		double distancia = 7.75;
+		int tiempoEsperadoMinutos = 10;
+
+		carrera = new Carrera(tarjetaCredito);
+		carrera.setOrigen(origen);
+		carrera.setDestino(destino);
+		carrera.setDistancia(distancia);
+		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+    }
+        
+    @Test
+    public void testCarreraGetCosteEsperado() {
+        double expResult = 11.17;
+        assertEquals(expResult, Tarifa.getCosteTotalEsperado(carrera), 0);
+    }
+}
+    
